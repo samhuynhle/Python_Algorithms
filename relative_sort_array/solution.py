e32c6fafd849648eacae2cae4711cc933cd5e3ab
samhuynhle <samhuynhle@gmail.com>
2020-02-14 18:19:00 -0800

    worked on fixng partition for quicksort in relative sort algorithm

--- relative_sort_array/solution.py
+++ relative_sort_array/solution.py
@@ -2,12 +2,11 @@ class Solution:
     def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
         left = 0
         
-        
         for i in arr2:
             left = self.relativeSort(arr1, i, left)
         
-        # self.quickSort(arr1, left, len(arr1) - 1)
-        arr1[left:] = sorted(arr1[left:])
+        self.quickSort(arr1, left, len(arr1) - 1)
+        # arr1[left:] = sorted(arr1[left:])
         
         return arr1
     
@@ -25,23 +24,24 @@ class Solution:
         return left
     
     def quickSort(self, arr1, left, right):
+        left  = left
+        right = right
+        
         if left < right:
             pi = self.partition(arr1, left, right)
             
             self.quickSort(arr1, left, pi-1)
             self.quickSort(arr1, pi+1, right)
             
-#     def partition(self, arr1, left, right):
-#         pivot = arr1[right]
-        
-#         left = left - 1
-#         right = right
-        
-#         while(left <= right):   
-#             while arr1[left] < pivot:
-#                 left += 1
-#             while arr1[right] > pivot:
-#                 right -= 1
+    
+    def partition(self,arr,left,right):
+        index = left - 1
+        pivot = arr[right]
 
-#             arr1[left], arr1[right] = arr1[right], arr1[left]
-#         return left
+        for j in range(left , right):
+            if   arr[j] <= pivot: 
+                index = index+1 
+                arr[index],arr[j] = arr[j],arr[index] 
+
+        arr[index+1],arr[right] = arr[right],arr[index+1] 
+        return index + 1 
